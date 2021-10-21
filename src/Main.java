1e57f9be60c10d620dc01a76ecec2579fa863623
George <symyon89@yahoo.com>
2021-10-21 19:55:50 +0300

    created classes and interface

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
+/*
+        clasa Error si clasele care extind Error intra in categoria
+        exceptiilor neverificate si se numesc erori (dpdv tehnic java)          Errors
 
+        tot in categoria unchecked exception intra si
+        exceptia RuntimeException (si toate clasele care o extind)              Runtime exceptions
+
+        *unchecked exceptions*
+
+        toate celelalte exceptii se numesc exceptii verificate                  Exceptions
+
+        *checked exceptions*
+
+     */
 public class Main {
     public static void main(String[] args) {
         User user = new User();
